c27d0cc263b940ab4a628b1946a3707ac3325e1e
Joe Fiorini <joe@joefiorini.com>
2015-10-06 11:39:02 -0400

    Adapt store for redux

--- js/store.js
+++ js/store.js
@@ -3,7 +3,32 @@
 
     // some variables and helpers for our fake database stuff
     var todoCounter = 0,
+        primaryState = getInitialState('todos'),
         localStorageKey = "todos";
+
+    function getInitialState(key) {
+        var loadedList = localStorage.getItem(key)
+        var list;
+
+        if (!loadedList) {
+            // If no list is in localstorage, start out with a default one
+            list = [{
+                key: todoCounter++,
+                created: new Date(),
+                isComplete: false,
+                label: 'Rule the web'
+            }];
+        } else {
+            list = _.map(JSON.parse(loadedList), function(item) {
+                // just resetting the key property for each todo item
+                item.key = todoCounter++;
+                return item;
+            });
+        }
+
+        return list;
+
+    }
 
     function getItemByKey(list,itemKey){
         return _.find(list, function(item) {
@@ -11,74 +36,109 @@
         });
     }
 
-    global.todoListStore = Reflux.createStore({
+    function getPayloadForAction(name, args) {
+      switch (name) {
+        case 'addItem':
+          return { label: args[0] };
+          break;
+        case 'toggleItem':
+        case 'removeItem':
+          return { itemKey: args[0] };
+        case 'editItem':
+          return { itemKey: args[0], newLabel: args[1] };
+        case 'toggleAllItems':
+          return { checked: args[0] };
+        default:
+          return {};
+      }
+
+    }
+
+    function reducerHandler(name, handler) {
+      return function(itemKey, newLabel) {
+
+        var args = Array.prototype.slice.apply(arguments);
+        var payload = getPayloadForAction(name, args);
+
+        // var args = [  primaryState,
+        //               { itemKey: itemKey, newLabel: newLabel }
+        //            ];
+        var newState = handler.apply(this, [primaryState, payload]);
+
+        localStorage.setItem(localStorageKey, JSON.stringify(newState));
+        // if we used a real database, we would likely do the below in a callback
+        primaryState = newState;
+        this.trigger(primaryState); // sends the updated list to all listening components (TodoApp)
+      };
+    }
+
+    function adaptStore(storeObject) {
+
+      var finalObject = {};
+
+      for (var key in storeObject) {
+        if (!storeObject.hasOwnProperty(key)) {
+          continue;
+        }
+
+        if (key === 'listenables' || key === 'updateList' || key === 'getInitialState') {
+          finalObject[key] = storeObject[key];
+          continue;
+        }
+
+        var firstLetter = key.slice(0, 1);
+        var rest = key.slice(1, key.length);
+        var newKey = 'on' + firstLetter.toUpperCase() + rest;
+        finalObject[newKey] = reducerHandler(key, storeObject[key]);
+      }
+
+      return Reflux.createStore(finalObject);
+    }
+
+    global.todoListStore = adaptStore({
         // this will set up listeners to all publishers in TodoActions, using onKeyname (or keyname) as callbacks
         listenables: [TodoActions],
-        onEditItem: function(itemKey, newLabel) {
-            var foundItem = getItemByKey(this.list,itemKey);
+        editItem: function(state, payload) {
+            var foundItem = getItemByKey(state, payload.itemKey);
             if (!foundItem) {
                 return;
             }
-            foundItem.label = newLabel;
-            this.updateList(this.list);
+            foundItem.label = payload.newLabel;
+            return state;
         },
-        onAddItem: function(label) {
-            this.updateList([{
-                key: todoCounter++,
-                created: new Date(),
-                isComplete: false,
-                label: label
-            }].concat(this.list));
+        addItem: function(state, payload) {
+          return [{ key: todoCounter++,
+                    created: new Date(),
+                    isComplete: false,
+                    label: payload.label
+                }].concat(state);
         },
-        onRemoveItem: function(itemKey) {
-            this.updateList(_.filter(this.list,function(item){
-                return item.key!==itemKey;
-            }));
+        removeItem: function(state, payload) {
+            return _.filter(state,function(item){
+                return item.key!==payload.itemKey;
+            });
         },
-        onToggleItem: function(itemKey) {
-            var foundItem = getItemByKey(this.list,itemKey);
+        toggleItem: function(state, payload) {
+            var foundItem = getItemByKey(state,payload.itemKey);
             if (foundItem) {
                 foundItem.isComplete = !foundItem.isComplete;
-                this.updateList(this.list);
             }
+            return state;
         },
-        onToggleAllItems: function(checked) {
-            this.updateList(_.map(this.list, function(item) {
-                item.isComplete = checked;
+        toggleAllItems: function(state, payload) {
+            return _.map(state, function(item) {
+                item.isComplete = payload.checked;
                 return item;
-            }));
+            });
         },
-        onClearCompleted: function() {
-            this.updateList(_.filter(this.list, function(item) {
+        clearCompleted: function(state) {
+            return _.filter(state, function(item) {
                 return !item.isComplete;
-            }));
-        },
-        // called whenever we change a list. normally this would mean a database API call
-        updateList: function(list){
-            localStorage.setItem(localStorageKey, JSON.stringify(list));
-            // if we used a real database, we would likely do the below in a callback
-            this.list = list;
-            this.trigger(list); // sends the updated list to all listening components (TodoApp)
+            });
         },
         // this will be called by all listening components as they register their listeners
         getInitialState: function() {
-            var loadedList = localStorage.getItem(localStorageKey);
-            if (!loadedList) {
-                // If no list is in localstorage, start out with a default one
-                this.list = [{
-                    key: todoCounter++,
-                    created: new Date(),
-                    isComplete: false,
-                    label: 'Rule the web'
-                }];
-            } else {
-                this.list = _.map(JSON.parse(loadedList), function(item) {
-                    // just resetting the key property for each todo item
-                    item.key = todoCounter++;
-                    return item;
-                });
-            }
-            return this.list;
+            return primaryState;
         }
     });
 
